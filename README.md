cd2374dd44ac39a2df7dca4b356c2338787fc7d2
AdinoFX <ubongabasiebebe41@gmail.com>
2024-08-27 16:45:26 +0100

    I worked on object in Javascript

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This repo is everything I'll be learning in JavaScript from beginner to advance level
+
+# OBJECT
+When working with the "this" keyword, I discovered that you cannot use the arrow function with it.
--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-class EssoStaffs {
-    constructor(name, age, role){
-        this.name = name,
-        this.age = age,
-        this.role = role
-    }
-    staffDetails(){
-        console.log(`Staff Name: ${this.name}`);
-        console.log(`Staff Age: ${this.age}`);
-        console.log(`Staff Role: ${this.role}`);
-        console.log(`******************************`);
-    }
-    set name(newName){
-        if(typeof newName === "string" && newName.length > 0){
-            this._name = newName;
-        }else{
-            console.error("Name should contain alphabets");
-        }
+//object
+
+const aboutMe ={
+    firstName: "Ubongabasi",
+    lastName: "Ebebe",
+    nickName: "AdinoFx",
+    isEmployed: true,
+    sayHello: function() {
+        console.log(`Name: ${this.firstName} ${this.lastName}`);
+        console.log(`Client Nickname: ${this.nickName}`);
+        console.log(`Is client employed: ${this.isEmployed}`);
     }
 }
 
-const staff1 = new EssoStaffs("due", "GUY", 9890);
-const staff2 = new EssoStaffs("Francis Idowu", 26, "Lifestyle Videography");
 
-staff1.staffDetails();
-staff2.staffDetails();
+aboutMe.sayHello();
